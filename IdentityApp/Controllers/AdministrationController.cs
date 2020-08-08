69ae811ac098970386ee3fbbfab1ecf88d8d1741
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-08-08 08:33:45 +0200

    Get list of roles

--- IdentityApp/Controllers/AdministrationController.cs
+++ IdentityApp/Controllers/AdministrationController.cs
@@ -37,7 +37,7 @@ namespace IdentityApp.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    return RedirectToAction("ListRoles", "Administration");
                 }
 
                 foreach(IdentityError error in result.Errors)
@@ -47,6 +47,13 @@ namespace IdentityApp.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
         }
     }
 }
